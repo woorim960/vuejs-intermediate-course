d46acea4d5d003c5b89d82addeaef5608b8fdace
woorim960 <woorimprog@gmail.com>
2022-07-28 17:32:34 +0900

    푸터 디자인

--- vue-todo-v2/src/components/TodoFooter.vue
+++ vue-todo-v2/src/components/TodoFooter.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    footer
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearAll">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    clearAll: function() {
+      localStorage.clear();
+    }
+  }
 }
 </script>
 
 <style>
+  .clearAllContainer {
+    width: 8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto;
+  }
 
+  .clearAllContainer:hover {
+    cursor: pointer;
+  }
+
+  .clearAllBtn {
+    color: #e20303;
+    display: block;
+  }
 </style>
