c8a53b5c6921d52cf90b3c29a8410a152f6434a3
woorim960 <woorimprog@gmail.com>
2022-07-28 16:27:52 +0900

    로컬스토리지 데이터 li태그로 띄우기

--- vue-todo-v2/src/components/TodoList.vue
+++ vue-todo-v2/src/components/TodoList.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todo in todoItems" v-bind:key="todo">{{ todo }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: [],
+    }
+  },
+  created: function() {
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) !== 'loglevel:webpack-dev-server')
+        this.todoItems.push(localStorage.key(i));
+    }
+  }
 }
 </script>
 
